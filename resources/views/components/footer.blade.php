4e2ca1feff020e5c2b7c357b3b3db70ed60fda33
Sean-Luca <r1043155@student.thomasmore.be>
2026-03-24 22:52:37 +0100

    added the link to the github in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,5 @@
-<footer class="h-[15vh] w-[100%] border-t-2 border-black dark:border-white bg-granite-600">
-    test
+<footer class="relative h-[15vh] w-full border-t-2 border-black dark:border-white bg-granite-600">
+   <ul class="absolute bottom-0 p-5 block w-full bg-granite-700/50 font-header [&_li]:hover:text-sandy-brown-300 [&_li]:transition [&_li]:duration-200ms">
+       <li><a href="https://github.com/Sean-Luca133/Portfolio " target="_blank">Github</a></li>
+   </ul>
 </footer>
